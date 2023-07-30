ffaeb63bccfaf85f9fe5397bd981cb3f86c2e2cf
Juan Manuel Gimenez <juanmauel20@gmail.com>
2023-07-30 00:11:23 -0300

    se agrego puerto y funciones middleware

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import express from 'express';
-
+import cors from 'cors';
 
 console.log('hola funciona el backend');
+
+const app = express();
+app.set("PORT", process.env.PORT || 4000) // PUERTO PROVISORIO de trabajo
+
+app.listen(app.get('PORT'), ()=>{
+console.log('puerto de trabajo   ' + app.get('PORT'))
+});
+// express.raw()
+// express.Router()
+// express.static()
+// express.text()
+// express.urlencoded()
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
