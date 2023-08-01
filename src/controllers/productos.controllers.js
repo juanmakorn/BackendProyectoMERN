0d7ebeba259f5ece1e84f52cf36e1eb1c126c724
Nicolas Powell <nicopowell30@gmail.com>
2023-08-01 11:53:21 -0300

    agregué el controlador de editarProducto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -22,3 +22,17 @@ export const crearProducto = async (req, res) => {
         });
     }
 };
+
+export const editarProducto = async (req, res) => {
+    try {
+        await Producto.findByIdAndUpdate(req.params.id, req.body);
+        res.status(200).json({
+            mensaje: "El producto fue actualizado correctamente",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Error, no se pudo borrar el producto",
+        });
+    }
+};
