d125d4fcc2838b4d02287b9e2b290a1bb473aea7
Nicolas Powell <nicopowell30@gmail.com>
2023-07-31 23:11:27 -0300

    agregue el modelo del producto

--- src/models/producto.js
+++ src/models/producto.js
@@ -1,8 +1,41 @@
 import mongoose, { Schema } from "mongoose";
 
 const productoSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true,
+        unique: true,
+        minLenght: 2,
+        maxLenght: 100,
+    },
+    estado: {
+        type: String,
+        required: true,
+        minLenght: 2,
+        maxLenght: 10,
+    },
+    precio: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 10000,
+    },
+    detalle: {
+        type: String,
+        required: true,
+        minLenght: 2,
+        maxLenght: 250,
+    },
+    imagen: {
+        type: String,
+        required: true,
+    },
+    categoria: {
+        type: String,
+        required: true,
+    },
 });
 
-const Producto = mongoose.model('producto', productoSchema)
+const Producto = mongoose.model("producto", productoSchema);
 
 export default Producto;
