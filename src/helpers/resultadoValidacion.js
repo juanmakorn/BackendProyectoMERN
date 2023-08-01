dea7ec9a5166bbe6b0ecdf0df44c6aaa8446f7df
Nicolas Powell <nicopowell30@gmail.com>
2023-07-31 23:05:54 -0300

    agregue el controlador de crearProducto

--- src/helpers/resultadoValidacion.js
+++ src/helpers/resultadoValidacion.js
@@ -3,13 +3,11 @@ import { validationResult } from "express-validator";
 const resultadoValidacion = (req, res, next) => {
     const errors = validationResult(req);
 
-    //errors.isEmpty(), true:esta vacio, false:Tiene errores
     if (!errors.isEmpty()) {
         return res.status(400).json({
             errores: errors.array(),
         });
     }
-    //Continuar con la ejecucion
     next();    
 }
 
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -2,4 +2,26 @@ import { Router } from "express";
 
 const router = Router();
 
+export const crearProducto = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errores: errors.array(),
+            });
+        }
+
+        const productoNuevo = new Producto(req.body);
+        await productoNuevo.save();
+        res.status(201).json({
+            mensaje: "El producto fue creado correctamente",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Error al intentar crear un producto",
+        });
+    }
+};
+
 export default router;
