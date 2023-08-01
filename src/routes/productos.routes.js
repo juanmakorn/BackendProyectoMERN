7bc713020e2284cd4a0fa3d7f91276f4d55c7940
Nicolas Powell <nicopowell30@gmail.com>
2023-08-01 11:54:13 -0300

    agregué la ruta para editarProducto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import validarProducto from "../helpers/validarProducto";
-import { crearProducto } from "../controllers/productos.controllers";
+import { crearProducto, editarProducto } from "../controllers/productos.controllers";
 
 const router = Router();
 
 router.route("/").post(validarProducto, crearProducto)
+router.route("/productos/:id").put(editarProducto);
 
 
 export default router;
