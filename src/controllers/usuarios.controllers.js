de3167239869f65b5e445ae829c3e0bffc7743bb
JulianIgnacio <montenegrojulianig@gmail.com>
2023-08-02 01:24:34 -0300

    armada estructura login inicial

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -26,5 +26,33 @@ export const obtenerUsuario = async (req, res) => {
 };
 
 export const login = async (req,res) => {
-    
+    try {
+        const {email, password} = req.body;
+
+        let usuario = await Usuario.findOne({email});
+        if(!usuario){
+
+          return res.status(404).json({
+            mensaje: 'Correo o password invalido - correo'
+          })
+        }
+
+        const passwordValido = bcrypt.compareSync(password, usuario.password);
+
+        if(!passwordValido){
+          return res.status(404).json({
+            mensaje: 'Correo o password invalido - password'
+          })
+        }
+
+        res.status(200).json({
+          mensaje: 'El usuario es correcto',
+          nombreUsuario: usuario.nombreUsuario
+        })
+      } catch (error) {
+        console.log(error);
+        res.status(404).json({
+          mensaje: "Usuario o password incorrecto",
+        });
+      }
 }
