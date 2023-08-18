dfb2565bc7cd480d385ae3a93ecbd56784367f5b
JulianIgnacio <montenegrojulianig@gmail.com>
2023-08-18 00:54:04 -0300

    agregado archivo token-verify.js

---
+++ src/helpers/token-verify.js
@@ -0,0 +1,19 @@
+import jwt  from "jsonwebtoken";
+
+const generarJWT = (uid, nombre)=>{
+    return new Promise( (resolve, reject)=>{
+
+        const payload = {uid, nombre};
+
+        jwt.sign(payload,process.env.SECRET_JWT,{
+            expiresIn: '2h'
+        },(err, token)=>{
+            if(err){
+                reject('No se pudo generar el token');
+            }
+            resolve(token);
+        })
+    })
+}
+
+export default generarJWT
